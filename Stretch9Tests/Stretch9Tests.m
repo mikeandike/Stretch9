0a6b2dfa595ca6528c81ed4101267743c423ff0e
Michael Sacks <michaelsacks@gmail.com>
2015-05-13 09:33:36 -0600

    Initial Commit

---
+++ Stretch9Tests/Stretch9Tests.m
@@ -0,0 +1,40 @@
+//
+//  Stretch9Tests.m
+//  Stretch9Tests
+//
+//  Created by Michael Sacks on 5/13/15.
+//  Copyright (c) 2015 Michael Sacks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Stretch9Tests : XCTestCase
+
+@end
+
+@implementation Stretch9Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
